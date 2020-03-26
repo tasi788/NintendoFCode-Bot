93c6d27cbb67d02a1429dace4b73ad213765f05f
tasi788 <tdc@tdccc.com.tw>
2020-03-26 15:53:21 +0800

    add configure

---
+++ bot/functions/configure.py
@@ -0,0 +1,25 @@
+import os
+from configparser import ConfigParser
+
+configure = ConfigParser()
+
+
+def get(key: str, value: str, default: str = None):
+    try:
+        return configure.get(key, value)
+    except Exception:
+        return default
+
+
+def getint(key: str, value: str, default: int = None):
+    try:
+        return configure.getint(key, value)
+    except Exception:
+        return default
+
+
+def getfloat(key: str, value: str, default: float = None):
+    try:
+        return configure.getfloat(key, value)
+    except Exception:
+        return default
