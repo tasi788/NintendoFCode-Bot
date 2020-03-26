7b7c96ccdfaf59c34515c1463f5723e91fdddebc
tasi788 <tdc@tdccc.com.tw>
2020-03-26 15:52:48 +0800

    update handle

--- bot/handle.py
+++ bot/handle.py
@@ -1,15 +1,18 @@
 import logging
 import coloredlogs
 from pyrogram import Client
+from .functions import configure
 
-
+logger = logging.getLogger(__name__)
+level = configure.get('bot', 'logging')
+coloredlogs.install(level=level, logger=logger)
 class handle:
     def __init__(self):
         pass
 
     def run(self):
-        app = Client("anonymous_sumbit", config_file='pyrogram.ini',
-                     plugins=dict(root='./sumbitbot/plugins'), workers=128)
+        app = Client("nintendo", config_file='pyrogram.ini',
+                     plugins=dict(root='./bot/plugins'), workers=128)
         app.start()
         myself = app.get_me()
         name = myself.first_name + ' ' + myself.username
