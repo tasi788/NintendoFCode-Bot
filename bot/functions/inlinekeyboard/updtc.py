7e3b583d03c4af3f637e68c921f4885b8168b499
tdc <tdc@tdccc.com.tw>
2020-03-31 22:58:33 +0800

    add icon

--- bot/functions/inlinekeyboard/updtc.py
+++ bot/functions/inlinekeyboard/updtc.py
@@ -1,5 +1,9 @@
 from typing import Union
-from pyrogram import InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardButton, InlineKeyboardMarkup
+
+from pyrogram import (InlineKeyboardButton, InlineKeyboardMarkup,
+                      InlineQueryResultArticle, InputTextMessageContent)
+
+from .. import configure
 
 
 def inline_keyboard(price: int):
@@ -21,8 +25,9 @@ def updtc(price: int = 0):
         text = InputTextMessageContent('婐 4 北七，我不知道甚麼 4 數字')
         keyboard = None
 
+    money_icon = configure.get('ACNH', 'money')
     result = [
         InlineQueryResultArticle(
-            input_message_content=text, title='更新大頭菜價格', reply_markup=keyboard)
+            input_message_content=text, title='更新大頭菜價格', reply_markup=keyboard, thumb_url=money_icon)
     ]
     return result
