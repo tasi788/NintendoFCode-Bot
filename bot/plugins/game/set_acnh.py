7325a4bcc256613cbf1fc8cb880accf215c4004e
tdc <tdc@tdccc.com.tw>
2020-04-04 03:58:49 +0800

    fix error

--- bot/plugins/game/set_acnh.py
+++ bot/plugins/game/set_acnh.py
@@ -12,8 +12,5 @@ def check_func(_, inline_query: CallbackQuery):
 
 @Client.on_inline_query(Filters.create(check_func))
 def acnh(client: Client, inline_query: CallbackQuery):
-    data = inline_query.query.split()
-
     keyboard = inlinekeyboard.bindacnh(inline_query)
-
     inline_query.answer(keyboard, cache_time=0)
