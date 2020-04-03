da44c6e0b8963e28634319a567c1fd512c6ce50a
tdc <tdc@tdccc.com.tw>
2020-04-03 22:41:23 +0800

    改用輔助輸入

--- bot/functions/inlinekeyboard/veg.py
+++ bot/functions/inlinekeyboard/veg.py
@@ -4,21 +4,9 @@ from pyrogram import (InlineKeyboardButton, InlineKeyboardMarkup,
 from .. import configure
 
 
-def inline_keyboard(user_id: int, price: int):
-    keyboard = [
-        [
-            InlineKeyboardButton(
-                '✅ 確定', callback_data='set acnh veg {price} {user_id}'.format(price=price, user_id=user_id))
-        ]
-    ]
-    return InlineKeyboardMarkup(keyboard)
-
-
 def veg(user_id: int = 0, price: int = 0):
     if price:
-        text = InputTextMessageContent(
-            '我的大頭菜價格是：`{price}`'.format(price=price), parse_mode='markdown')
-        keyboard = inline_keyboard(user_id, price)
+        text = InputTextMessageContent('/acnh veg {price}'.format(price=price))
     else:
         text = InputTextMessageContent('婐 4 北七，我不知道甚麼 4 數字')
         keyboard = None
@@ -26,6 +14,6 @@ def veg(user_id: int = 0, price: int = 0):
     money_icon = configure.get('ACNH', 'money')
     result = [
         InlineQueryResultArticle(
-            input_message_content=text, title='更新大頭菜價格', reply_markup=keyboard, thumb_url=money_icon)
+            input_message_content=text, title='更新大頭菜價格', thumb_url=money_icon)
     ]
     return result
