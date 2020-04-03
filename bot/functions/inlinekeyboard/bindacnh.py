0a4450ea35c8d5c34198d5e60104a3e34fb1a5a0
tdc <tdc@tdccc.com.tw>
2020-04-04 03:58:16 +0800

    設下關卡檢查拉

--- bot/functions/inlinekeyboard/bindacnh.py
+++ bot/functions/inlinekeyboard/bindacnh.py
@@ -21,9 +21,13 @@ def bindacnh(callback_query: CallbackQuery):
     else:
         island = data[2]
         fruit = data[3]
-        long = '/bindgame acnh -n {name} -f {fruit}'.format(
-            name=island, fruit=fruit)
-        short = '輸入好的話就點我吧狸！'
+        if fruit not in ['梨子', '櫻桃', '蘋果', '桃子', '橘子']:
+            short = '水果種類有誤，請檢查下狸！'
+            long = short + '\n每個人島上的水果特產種類應該是 「梨子、櫻桃、蘋果、桃子、橘子」 其中一種喔！'
+        else:
+            long = '/bindgame acnh -n {name} -f {fruit}'.format(
+                name=island, fruit=fruit)
+            short = '輸入好的話就點我吧狸！'
 
     text = InputTextMessageContent(long)
     keyboard = InlineQueryResultArticle(
