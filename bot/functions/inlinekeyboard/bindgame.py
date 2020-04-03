ad114677382a953464cff6f6bc05315c2258e0b4
tdc <tdc@tdccc.com.tw>
2020-04-03 23:48:08 +0800

    base url

--- bot/functions/inlinekeyboard/bindgame.py
+++ bot/functions/inlinekeyboard/bindgame.py
@@ -5,14 +5,13 @@ from ...game import gamelist
 
 
 def bindgame():
+    base_url = 'https://raw.githubusercontent.com/tasi788/NintendoFCode-Bot/%E5%A4%A7%E9%A0%AD%E8%8F%9C%E5%8A%9F%E8%83%BD/bot/statics/games/'
     result = list()
 
     for game in gamelist():
         text = InputTextMessageContent(
             '/bindgame {short}'.format(short=game.short))
-        print(
-            'https://raw.githubusercontent.com/tasi788/NintendoFCode-Bot/%E5%A4%A7%E9%A0%AD%E8%8F%9C%E5%8A%9F%E8%83%BD/bot/statics/games/{pics}'.format(pics=game.pics))
         keyboard = InlineQueryResultArticle(
-            input_message_content=text, title=game.name, thumb_url='https://raw.githubusercontent.com/tasi788/NintendoFCode-Bot/%E5%A4%A7%E9%A0%AD%E8%8F%9C%E5%8A%9F%E8%83%BD/bot/statics/games/{pics}'.format(pics=game.pics))
+            input_message_content=text, title=game.name, thumb_url='{base_url}{pics}'.format(base_url=base_url, pics=game.pics))
         result.append(keyboard)
     return result
