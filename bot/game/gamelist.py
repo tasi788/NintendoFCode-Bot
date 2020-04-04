4f2de2451d9b421ec8664fdee5a34c53fae1c73d
tdc <tdc@tdccc.com.tw>
2020-04-04 13:32:28 +0800

    make a quick search func

--- bot/game/gamelist.py
+++ bot/game/gamelist.py
@@ -5,10 +5,17 @@ from dacite import from_dict
 from ..types import game
 
 
-def gamelist():
-    glist = list()
+def gamelist(game: str):
     file = open('./bot/game/gamelist.json', 'r').read()
     loads = json.loads(file)
-    for g in loads:
-        glist.append(from_dict(data_class=game.Game, data=g))
+    if game:
+        for g in loads:
+            if game == g['short']:
+                glist = from_dict(data_class=game.Game, data=g)
+            else:
+                glist = None
+    else:
+        glist = list()
+        for g in loads:
+            glist.append(from_dict(data_class=game.Game, data=g))
     return glist
