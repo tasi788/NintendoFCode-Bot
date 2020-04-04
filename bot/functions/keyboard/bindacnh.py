8574e08038a48f0ac884f1edf48d10b7f82f9583
tdc <tdc@tdccc.com.tw>
2020-04-04 14:31:06 +0800

    make more condition

--- bot/functions/keyboard/bindacnh.py
+++ bot/functions/keyboard/bindacnh.py
@@ -1,15 +1,25 @@
 from pyrogram import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-def bindacnh(share=False):
+def bindacnh(share=False, visble=False):
+    keyboard = list()
     if share:
-        text = '我也想綁定動森友資料！'
-    else:
+        text = '分享到群組'
+        keyboard.append(
+            [InlineKeyboardButton(
+                text, switch_inline_query='myacnh')]
+        )
+    if visble:
+        text = '我也要綁定動物森友會資料！'
+        keyboard.append(
+            [InlineKeyboardButton(
+                text, url='t.me/NintendoFCode_bot?start=help')]
+        )
+    if not share and not visble:
         text = '🔗 開始綁定'
-    keyboard = [
-        [
-            InlineKeyboardButton(
-                text, switch_inline_query_current_chat='bindgame acnh')
-        ]
-    ]
+        keyboard.append(
+            [InlineKeyboardButton(
+                text, switch_inline_query_current_chat='bindgame acnh')]
+        )
+
     return InlineKeyboardMarkup(keyboard)
