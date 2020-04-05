d63209d679000dad4b823141f3e52c27daaeed15
tdc <tdc@tdccc.com.tw>
2020-04-05 15:54:36 +0800

    fix typo

--- bot/plugins/helpme.py
+++ bot/plugins/helpme.py
@@ -28,7 +28,7 @@ def helpme(client: Client, message: Message):
         '顯示自己的好友代碼\n' \
         '範例: `/myfc`\n\n' \
         '隱私設定，決定是否讓好友代碼被他人查詢\n' \
-        '範例: `/findfc`\n\n' \
+        '範例: `/privacy`\n\n' \
         '綁定特定遊戲，目前僅支援動物森友會\n' \
         '範例：`/bindgame`\n\n' \
         '此工具啟發自 @NintendoFCPool_bot 並加以延伸。'
